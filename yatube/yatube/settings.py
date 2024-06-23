2fa2b8360f7057c88da94d16adeabce1637eb798
Евгений Москалянов <emos@inbox.ru>
2024-06-23 21:25:56 +0300

    Убрал из настроек лишнее.

--- yatube/yatube/settings.py
+++ yatube/yatube/settings.py
@@ -31,12 +31,14 @@ ALLOWED_HOSTS = [
     '127.0.0.1',
     '[::1]',
     'testserver',
+    # 'www.yamos.pythonanywhere.com',
+    # 'yamos.pythonanywhere.com',
 ]
 
 
 INTERNAL_IPS = [
     '127.0.0.1',
-] 
+]
 
 
 # Application definition
@@ -53,7 +55,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'sorl.thumbnail',
-    'debug_toolbar',
+    # 'debug_toolbar',
 ]
 
 MIDDLEWARE = [
@@ -64,7 +66,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'debug_toolbar.middleware.DebugToolbarMiddleware',
+    # 'debug_toolbar.middleware.DebugToolbarMiddleware',
 ]
 
 ROOT_URLCONF = 'yatube.urls'
